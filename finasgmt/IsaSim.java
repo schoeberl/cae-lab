0ffc4d85be4d2bc08d807aaa001b48e6aa1b2860
Martin Schoeberl <martin@jopdesign.com>
2017-10-27 14:39:57 +0200

    A simple start for a RISC-V simulator

---
+++ finasgmt/IsaSim.java
@@ -0,0 +1,61 @@
+/**
+ * RISC-V Instruction Set Simulator
+ * 
+ * A tiny first step to get the simulator started. Can execute just a single
+ * RISC-V instruction.
+ * 
+ * @author Martin Schoeberl (martin@jopdesign.com)
+ *
+ */
+public class IsaSim {
+
+	static int pc;
+	static int reg[] = new int[4];
+
+	// Here the first program hard coded as an array
+	static int progr[] = {
+			// As minimal RISC-V assembler example
+			0x00200093, // addi x1 x0 2
+			0x00300113, // addi x2 x0 3
+			0x002081b3, // add x3 x1 x2
+	};
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello RISC-V World!");
+
+		pc = 0;
+
+		for (;;) {
+
+			int instr = progr[pc];
+			int opcode = instr & 0x7f;
+			int rd = (instr >> 7) & 0x01f;
+			int rs1 = (instr >> 15) & 0x01f;
+			int imm = (instr >> 20);
+
+			switch (opcode) {
+
+			case 0x13:
+				reg[rd] = reg[rs1] + imm;
+				break;
+			default:
+				System.out.println("Not yet iomplemented");
+				break;
+			}
+
+			++pc; // We count in 4 byte words
+			if (pc >= progr.length) {
+				break;
+			}
+			for (int i = 0; i < reg.length; ++i) {
+				System.out.print(reg[i] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("Program exit");
+
+	}
+
+}
