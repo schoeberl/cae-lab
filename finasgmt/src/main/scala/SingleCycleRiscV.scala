bb578409cf58c1df3c1c9ac30222235ac971de10
Martin Schoeberl <martin@jopdesign.com>
2020-09-30 18:16:18 +0200

    finasgmt: simplify Chisel version

--- finasgmt/src/main/scala/SingleCycleRiscV.scala
+++ finasgmt/src/main/scala/SingleCycleRiscV.scala
@@ -13,7 +13,14 @@ class SingleCycleRiscV extends Module {
   })
 
   val pc = RegInit(0.U(32.W))
-  val reg = Reg(Vec(4, UInt(32.W)))
+
+  // TODO: this is ugly, there should be an elegant way to express this
+  val vec = Wire(Vec(4, UInt(32.W)))
+  vec(0) := 0.U
+  vec(1) := 0.U
+  vec(2) := 0.U
+  vec(3) := 0.U
+  val reg = RegInit(vec)
 
   // This program should be read in from a file
   val program = Array[Int](
@@ -21,25 +28,12 @@ class SingleCycleRiscV extends Module {
   0x00300113, // addi x2 x0 3
   0x002081b3) // add x3 x1 x2
 
-
   // A little bit of functional magic to convert the Int Array for a Vec of UInt
   val imem = VecInit(program.map(_.U(32.W)))
 
   reg(0) := 0.U
 
-  // print the register contents
-  // Maybe this should
-  def printReg(): Unit = {
-    for (i <- 0 until 4) {
-      printf("%d ", reg(i))
-    }
-    printf("\n")
-  }
-
   val instr = imem(pc(31, 2))
-  // printf starts in second clock cycle, first clock cycle is not shown
-  // printf("instr: %x\n", instr)
-  // printReg()
 
   val opcode = instr(6, 0)
   val rd = instr(11, 7)
--- finasgmt/src/test/scala/SingleCycleRiscVTester.scala
+++ finasgmt/src/test/scala/SingleCycleRiscVTester.scala
@@ -1,27 +1,23 @@
 import chisel3.iotesters._
 import org.scalatest._
 
-
-class SingleCycleRiscVTester(dut: SingleCycleRiscV) extends PeekPokeTester(dut) {
-
-  def printReg() = {
-    for (i <- 0 until 4) {
-      print(peek(dut.io.regDeb(i)).toString + " ")
-    }
-    println()
-  }
-
-
-  for (i <- 0 until 3) {
-    printReg()
-    step(1)
-  }
-}
-
 class RiscVSpec extends FlatSpec with Matchers {
   "RiscV" should "pass" in {
-    chisel3.iotesters.Driver(() => new SingleCycleRiscV()) {
-      c => new SingleCycleRiscVTester(c)
-    } should be (true)
+    chisel3.iotesters.Driver(() => new SingleCycleRiscV()) { c =>
+      new PeekPokeTester(c) {
+
+        def printReg() = {
+          for (i <- 0 until 4) {
+            print(peek(dut.io.regDeb(i)).toString + " ")
+          }
+          println()
+        }
+
+        for (i <- 0 until 3) {
+          printReg()
+          step(1)
+        }
+      }
+    }
   }
 }
