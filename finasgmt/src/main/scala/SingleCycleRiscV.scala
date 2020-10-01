8cff6c2b5770ebfece93704ef583e49a55627d4a
Martin Schoeberl <martin@jopdesign.com>
2020-10-01 12:15:51 +0200

    finasgmt: simplify the Chisel template

--- finasgmt/src/main/scala/SingleCycleRiscV.scala
+++ finasgmt/src/main/scala/SingleCycleRiscV.scala
@@ -7,31 +7,30 @@ import chisel3.util._
   * A single-cycle implementation of RISC-V is practically an ISA simulator.
   */
 class SingleCycleRiscV extends Module {
-  val io = IO(new Bundle{
+  val io = IO(new Bundle {
     val regDeb = Output(Vec(4, UInt(32.W))) // debug output for the tester
     val done = Output(Bool())
   })
 
-  val pc = RegInit(0.U(32.W))
 
-  // TODO: this is ugly, there should be an elegant way to express this
-  val vec = Wire(Vec(4, UInt(32.W)))
-  vec(0) := 0.U
-  vec(1) := 0.U
-  vec(2) := 0.U
-  vec(3) := 0.U
-  val reg = RegInit(vec)
-
-  // This program should be read in from a file
+  // TODO: the program should be read in from a file
   val program = Array[Int](
-  0x00200093, // addi x1 x0 2
-  0x00300113, // addi x2 x0 3
-  0x002081b3) // add x3 x1 x2
+    0x00200093, // addi x1 x0 2
+    0x00300113, // addi x2 x0 3
+    0x002081b3) // add x3 x1 x2
 
-  // A little bit of functional magic to convert the Int Array for a Vec of UInt
+  // A little bit of functional magic to convert the Scala Int Array to a Chisel Vec of UInt
   val imem = VecInit(program.map(_.U(32.W)))
 
-  reg(0) := 0.U
+
+  val pc = RegInit(0.U(32.W))
+
+  // TODO: there should be an elegant way to express this
+  val vec = Wire(Vec(4, UInt(32.W)))
+  for (i <- 0 until 4) vec(i) := 0.U
+  // We initialize the register file to 0 for a nicer display
+  // In a real processor this is usually not done
+  val reg = RegInit(vec)
 
   val instr = imem(pc(31, 2))
 
@@ -41,19 +40,18 @@ class SingleCycleRiscV extends Module {
   val imm = instr(31, 20) // TODO sign extend
 
   switch(opcode) {
-    is (0x13.U) {
+    is(0x13.U) {
       reg(rd) := reg(rs1) + imm
     }
   }
 
   pc := pc + 4.U
 
-  // done should be set when the program ends => the tester shall stop
+  // done should be set when the program ends, and the tester shall stop
   io.done := true.B
 
-  for (i <- 0 until 4) {
-    io.regDeb(i) := reg(i)
-  }
+  // Make the register file visible to the tester
+  for (i <- 0 until 4) io.regDeb(i) := reg(i)
 }
 
 
--- finasgmt/src/test/scala/SingleCycleRiscVTester.scala
+++ finasgmt/src/test/scala/SingleCycleRiscVTester.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 
 class RiscVSpec extends FlatSpec with Matchers {
   "RiscV" should "pass" in {
-    chisel3.iotesters.Driver(() => new SingleCycleRiscV()) { c =>
+    chisel3.iotesters.Driver.execute(Array("--generate-vcd-output", "on"), () => new SingleCycleRiscV()) { c =>
       new PeekPokeTester(c) {
 
         def printReg() = {
@@ -13,7 +13,7 @@ class RiscVSpec extends FlatSpec with Matchers {
           println()
         }
 
-        for (i <- 0 until 3) {
+        for (i <- 0 until 4) {
           printReg()
           step(1)
         }
